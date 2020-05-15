38902895f32c052b28bf8099ee1577b5557e1d82
Ouzouigh Kamel <ouzouighkamel@gmail.com>
2020-05-15 16:46:47 +0200

    Inlining variables

--- src/main/groovy/refactoring/Statement.groovy
+++ src/main/groovy/refactoring/Statement.groovy
@@ -11,44 +11,49 @@ class Statement {
         def result = "Statement for ${invoice.customer}\n"
         def format = NumberFormat.getCurrencyInstance(new Locale("en", "US"))
         format.setMinimumFractionDigits(2)
+        def playFor = { aPerformance ->
+            plays[aPerformance.playID]
+        }
+        def amountFor =  { aPerformance ->
+            def amount = 0
+            switch (playFor(aPerformance).type) {
+                case "tragedy":
+                    amount = 40000
+                    if (aPerformance.audience > 30) {
+                        amount += 1000 * (aPerformance.audience - 30)
+                    }
+                    break
+                case "comedy":
+                    amount = 30000
+                    if (aPerformance.audience > 20) {
+                        amount += 10000 + 500 * (aPerformance.audience - 20)
+                    }
+                    amount += 300 * aPerformance.audience
+                    break
+                default:
+                    throw new Error("unknown type: ${playFor(aPerformance).type}")
+            }
+            amount
+        }
+
         for (def perf in invoice.performances) {
-            final def play = plays[perf.playID]
-            def thisAmount = amountFor(perf, play)
 
             // add volume credits
-            volumeCredits += Math.max(perf.audience - 30, 0);
+            volumeCredits += Math.max(perf.audience - 30, 0)
+
             // add extra credit for every ten comedy attendees
-            if ("comedy" == play.type) volumeCredits += Math.floor(perf.audience / 5)
+            if ("comedy" == playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5)
+
             // print line for this order
-            result += " ${play.name}: ${format.format(thisAmount / 100)} (${perf.audience} seats)\n"
-            totalAmount += thisAmount;
+            result += " ${playFor(perf).name}: ${format.format(amountFor(perf) / 100)} (${perf.audience} seats)\n"
+            totalAmount += amountFor(perf)
         }
         result += "Amount owed is ${format.format(totalAmount / 100)}\n"
         result += "You earned ${volumeCredits} credits\n"
-        return result;
+
+        return result
     }
 
-    private def amountFor(aPerformance, play) {
-        def result = 0
-        switch (play.type) {
-            case "tragedy":
-                result = 40000;
-                if (aPerformance.audience > 30) {
-                    result += 1000 * (aPerformance.audience - 30)
-                }
-                break
-            case "comedy":
-                result = 30000
-                if (aPerformance.audience > 20) {
-                    result += 10000 + 500 * (aPerformance.audience - 20)
-                }
-                result += 300 * aPerformance.audience
-                break
-            default:
-                throw new Error("unknown type: ${play.type}")
-        }
-        result
-    }
 
 
 }
