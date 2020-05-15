6f8277cc4b4daf415946d49517ad4d21187cdffe
Ouzouigh Kamel <ouzouighkamel@gmail.com>
2020-05-15 16:14:43 +0200

    Decomposing the statement function

--- src/main/groovy/refactoring/Statement.groovy
+++ src/main/groovy/refactoring/Statement.groovy
@@ -5,36 +5,16 @@ import java.text.NumberFormat
 
 class Statement {
 
-    def statement(def invoice, def plays) {
+    def statement(invoice, plays) {
         def totalAmount = 0
         def volumeCredits = 0
         def result = "Statement for ${invoice.customer}\n"
         def format = NumberFormat.getCurrencyInstance(new Locale("en", "US"))
         format.setMinimumFractionDigits(2)
-
-        /*def format = new Intl.NumberFormat("en-US",
-                { style: "currency", currency: "USD",
-                    minimumFractionDigits: 2 }).format;*/
         for (def perf in invoice.performances) {
             final def play = plays[perf.playID]
-            def thisAmount = 0
-            switch (play.type) {
-                case "tragedy":
-                    thisAmount = 40000;
-                    if (perf.audience > 30) {
-                        thisAmount += 1000 * (perf.audience - 30)
-                    }
-                    break
-                case "comedy":
-                    thisAmount = 30000
-                    if (perf.audience > 20) {
-                        thisAmount += 10000 + 500 * (perf.audience - 20)
-                    }
-                    thisAmount += 300 * perf.audience
-                    break
-                default:
-                    throw new Error("unknown type: ${play.type}")
-            }
+            def thisAmount = amountFor(perf, play)
+
             // add volume credits
             volumeCredits += Math.max(perf.audience - 30, 0);
             // add extra credit for every ten comedy attendees
@@ -46,6 +26,28 @@ class Statement {
         result += "Amount owed is ${format.format(totalAmount / 100)}\n"
         result += "You earned ${volumeCredits} credits\n"
         return result;
+    }
+
+    private def amountFor(aPerformance, play) {
+        def result = 0
+        switch (play.type) {
+            case "tragedy":
+                result = 40000;
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30)
+                }
+                break
+            case "comedy":
+                result = 30000
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20)
+                }
+                result += 300 * aPerformance.audience
+                break
+            default:
+                throw new Error("unknown type: ${play.type}")
+        }
+        result
     }
 
 
