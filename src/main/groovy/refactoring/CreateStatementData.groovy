481f4d017129ab06d0229fb21ec75379a0d382d8
Ouzouigh Kamel <ouzouighkamel@gmail.com>
2020-05-16 13:11:36 +0200

    Moving Functions into the Calculator

--- src/main/groovy/refactoring/CreateStatementData.groovy
+++ src/main/groovy/refactoring/CreateStatementData.groovy
@@ -6,40 +6,13 @@ class CreateStatementData {
     def playFor = { aPerformance ->
       plays[aPerformance.playID]
     }
-    def amountFor = { aPerformance ->
-      def result = 0
-      switch (aPerformance.play.type) {
-        case "tragedy":
-          result = 40000
-          if (aPerformance.audience > 30) {
-            result += 1000 * (aPerformance.audience - 30)
-          }
-          break
-        case "comedy":
-          result = 30000
-          if (aPerformance.audience > 20) {
-            result += 10000 + 500 * (aPerformance.audience - 20)
-          }
-          result += 300 * aPerformance.audience
-          break
-        default:
-          throw new Error("unknown type: ${aPerformance.play.type}")
-      }
-      result
-    }
-    def volumeCreditsFor = { aPerformance ->
-      def result = 0
-      result += Math.max(aPerformance.audience - 30, 0)
-      if ("comedy" == aPerformance.play.type) result += Math.floor(aPerformance.audience / 5)
-      result
-    }
     def enrichPerformance = { aPerformance ->
       final def calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance))
       def result = [:]
       result << aPerformance
       result.play = calculator.play
-      result.amount = amountFor(result)
-      result.volumeCredits = volumeCreditsFor(result)
+      result.amount = calculator.amount
+      result.volumeCredits = calculator.volumeCredits
       result
     }
 
